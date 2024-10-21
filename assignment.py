a5d6b73d43d07366a740ccc36f5113bc3fe390e6
Zacharyf-431 <zachary.fowler1@stu.ocsb.ca>
2024-10-21 01:30:03 +0000

    python thingy

--- assignment.py
+++ assignment.py
@@ -2,7 +2,8 @@
 # Task is calculating the Area and Circumference of a circle, using the user's input
 
 import math 
-x = float(input ())
-y = float(x / 2)
-print ("The Area is: ", float(math.pi * (x * 2))
-print ("The circumference is: ", float(math.pi * (x**2))
+x = float(input("Enter the radius: "))
+area = math.pi * (x ** 2)
+circumference = 2 * math.pi * x
+print("The Area is:", area)
+print("The Circumference is:", circumference)
