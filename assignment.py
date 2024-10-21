d04610322c59c02ab3e1e9b504c78fea0837efa1
Zacharyf-431 <zachary.fowler1@stu.ocsb.ca>
2024-10-21 00:46:42 +0000

    mdl layout

--- assignment.py
+++ assignment.py
@@ -1,1 +1,8 @@
+# My Python Program
+# Task is calculating the Area and Circumference of a circle, using the user's input
 
+import math 
+x = float(input ())
+y = float(x / 2)
+print ("The Area is: ", float(math.pi * (x * 2))
+print ("The circumference is: ", float(math.pi * (x**2))
